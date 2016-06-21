8b4fdddd1e6e6053be2f99172e43392b785e5210
Antonia10 <antoniaumari@gmail.com>
2016-06-21 16:03:57 -0300

    primeiro commit

---
+++ programaCalculadora.pas
@@ -0,0 +1,16 @@
+Program calculaNumero ;
+var
+		n1,n2,soma,subtracao,multiplicacao:integer;
+		divisao:real;
+Begin
+     write('Digite dois números:');
+     readln(n1,n2);
+     soma:=n1 + n2;
+     subtracao:=n1 - n2;
+     multiplicacao:=n1 * n2;
+     divisao:=n1 / n2;
+     writeln('A soma dos números é:', soma);
+     writeln('A subtração  dos números é:', subtracao);
+     writeln('A multiplicação dos números é:', multiplicacao);
+     writeln('A divisão dos números é:', divisao);
+End.
