49d271a6e47c81083d8d4c20df4c9e83c6c70e37
Antonia10 <antoniaumari@gmail.com>
2016-06-21 16:28:10 -0300

    segundo commit

--- programaCalculadora.pas
+++ programaCalculadora.pas
@@ -1,17 +1,31 @@
 Program calculaNumero ;
 var
+		operacao:char;
 		n1,n2,soma,subtracao,multiplicacao:integer;
 		divisao:real;
 Begin
      write('Digite dois números:');
      readln(n1,n2);
-     soma:=n1 + n2;
-     subtracao:=n1 - n2;
-     multiplicacao:=n1 * n2;
+     write('Digite a operação que gostaria de fazer:');
+     readln(operacao);
      divisao:=n1 / n2;
-     writeln('A soma dos números é:', soma);
-     writeln('A subtração  dos números é:', subtracao);
-     writeln('A multiplicação dos números é:', multiplicacao);
-     writeln('A divisão dos números é:', divisao:6:2);
+     case operacao of
+     			'+': begin
+					 				soma:=n1 + n2;
+								  writeln('A soma dos números é:', soma);
+								end;  
+          '-':begin
+                   subtracao:=n1 - n2;
+									 writeln('A subtração  dos números é:', subtracao);
+							end;		 
+    			'*':begin
+    								multiplicacao:=n1 * n2;
+									 writeln('A multiplicação dos números é:', multiplicacao);
+							end;		 
+     			'/': begin
+     			          divisao:=n1 / n2;
+					 				 writeln('A divisão dos números é:', divisao:6:2);
+					 			end;
+			end;					 	 
      readln;
-End.                                                    {lkjlkjlkjl}
+End.                                                    
